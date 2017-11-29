44314f19c625ac85465270ad3bafb0d1783b2fcb
Jacob Ross <jross1818@gmail.com>
2017-11-28 22:43:09 -0600

    added code to create users table

--- db.sql
+++ db.sql
@@ -8,3 +8,10 @@ CREATE TABLE `songs` (
     `genre` VARCHAR(100) NOT NULL DEFAULT '',
     PRIMARY KEY (`id`)
 )
+CREATE TABLE users (
+	userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	loginID varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	firstName varchar(128) NOT NULL,
+	lastName varchar(128) NOT NULL
+);
