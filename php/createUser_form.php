d964dff5e0f3a8b428afd5faf03ffc819b2ab24a
Jacob Ross <jross1818@gmail.com>
2017-11-30 20:35:19 -0600

    Added functionality for creating new users

--- php/createUser_form.php
+++ php/createUser_form.php
@@ -1,3 +1,32 @@
+<?php
+    require 'db.php';
+    
+    //functionality for creating a user and entering into the database
+    if(isset($_POST['submit'])){
+        //check if username is unique
+        $sql_check = "SELECT * FROM users WHERE loginID = ?";
+        $stmt = mysqli->prepare($sql_check);
+        $stmt->bind_param("s", $_POST['username']);
+        $stmt->execute();
+        
+        $stmt->store_result();
+        if($stmt->num_rows == 0){
+            $sql = "INSERT INTO users (loginID, password, firstName, lastName) VALUES (?,?,?,?)";
+            
+            $stmt = mysqli->prepare($sql);
+            
+            $stmt->bind_param("ssss", $_POST['username'], $_POST['password'], $_POST['firstName'], $_POST['lastName']);
+            
+            $stmt->execute();
+            $stmt->close();
+            header('login_form.php')
+        } else {
+            $error_flag = 1;
+	    $stmt->close();
+        }
+        
+    }
+?>
 <!DOCTYPE html>
 
 <html>
@@ -20,7 +49,7 @@
             }
         ?>
         
-        <form action="login.php" method="POST">
+        <form action="createUser_form.php" method="POST">
             
             <input type="hidden" name="action" value="do_login">
             
@@ -35,8 +64,9 @@
             </div>
             
             <div class="newUserInput">
+		<?php /*error flag for non-unique username*/ if($error_flag == 1){ echo "Username taken. Try again."; } ?>
                 <label for="username">User name:</label>
-                <input type="text" id="username" name="username" value="<?php print $username; ?>">
+                <input type="text" id="username" name="username" value="<?php print $username;?>">
             </div>
             
             <div class="newUserInput">
