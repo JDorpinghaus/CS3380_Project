147c87f5a4d4013fdc7f6dbcd8728333d5c4d12d
Mason <mrbrhc@mail.missouri.edu>
2017-12-05 13:06:30 -0600

    login status check

--- php/index.php
+++ php/index.php
@@ -1,4 +1,19 @@
 <?php
+    
+    if(!session_start()){
+        header("Location: error.php");
+        exit;
+    }
+	
+	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
+	
+	
+	if (!$loggedIn) {
+		header("Location: login.php");
+		exit;
+	}
+
+
     require("db.php");
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo file_get_contents("../html/index.html");
